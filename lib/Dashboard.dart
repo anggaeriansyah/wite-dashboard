619493a1f97470522331abc67bc17c0c140d3854
SearGean <anggaeriansyah43@gmail.com>
2023-04-18 19:20:36 +0700

    membuat tombol CRUD

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -42,75 +42,197 @@ class _DashboardState extends State<Dashboard> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    height: 120,
-                    width: MediaQuery.of(context).size.width * 0.42,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 3),
-                            blurRadius: 10,
-                            color: Colors.black12,
-                            // spreadRadius: 0.5,
-                          )
-                        ]),
-                    child: const Icon(Icons.add_box_rounded),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 3),
+                                blurRadius: 10,
+                                color: Colors.black12,
+                                // spreadRadius: 0.5,
+                              )
+                            ]),
+                      ),
+                      const Positioned(
+                          top: 15,
+                          right: 15,
+                          child: Icon(
+                            Icons.add_box_rounded,
+                            size: 30,
+                            color: Colors.white,
+                          )),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                            // color: Colors.amber,
+                            width: MediaQuery.of(context).size.width * 0.42,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: const Text(
+                              '''Tambah
+data wisata''',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                              overflow: TextOverflow.fade,
+                              softWrap: true,
+                              maxLines: 2,
+                            )),
+                      ),
+                    ],
                   ),
-                  Container(
-                      height: 120,
-                      width: MediaQuery.of(context).size.width * 0.42,
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 3),
-                              blurRadius: 10,
-                              color: Colors.black12,
-                              // spreadRadius: 0.5,
-                            )
-                          ]),
-                      child: const Icon(Icons.edit)),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        decoration: BoxDecoration(
+                            color: Colors.yellow.shade700,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            boxShadow: const [
+                              BoxShadow(
+                                offset: Offset(0, 3),
+                                blurRadius: 10,
+                                color: Colors.black12,
+                                // spreadRadius: 0.5,
+                              )
+                            ]),
+                      ),
+                      const Positioned(
+                          top: 15,
+                          right: 15,
+                          child: Icon(
+                            Icons.edit,
+                            size: 30,
+                            color: Colors.white,
+                          )),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                            // color: Colors.amber,
+                            width: MediaQuery.of(context).size.width * 0.42,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: const Text(
+                              '''Edit
+data wisata''',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                              overflow: TextOverflow.fade,
+                              softWrap: true,
+                              maxLines: 2,
+                            )),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
-            // Flexible(
-            //     child: Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 20),
-            //   child: Column(
-            //     children: [
-            //       Container(
-            //         color: Colors.amber,
-            //         child: const ListTile(
-            //           leading: Icon(Icons.add_box),
-            //           title: Text(
-            //             'Tambah data wisata',
-            //             style: TextStyle(
-            //                 fontSize: 16, fontWeight: FontWeight.w500),
-            //           ),
-            //         ),
-            //       ),
-            //       const SizedBox(height: 5),
-            //       Container(
-            //         color: Colors.amber,
-            //         child: const ListTile(
-            //           leading: Icon(Icons.edit),
-            //           title: Text('Edit data wisata'),
-            //         ),
-            //       ),
-            //       const SizedBox(height: 5),
-            //       Container(
-            //         color: Colors.amber,
-            //         child: const ListTile(
-            //           leading: Icon(Icons.delete),
-            //           title: Text('Hapus data wisata'),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        decoration: const BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 3),
+                                blurRadius: 10,
+                                color: Colors.black12,
+                                // spreadRadius: 0.5,
+                              )
+                            ]),
+                      ),
+                      const Positioned(
+                          top: 15,
+                          right: 15,
+                          child: Icon(
+                            Icons.delete,
+                            size: 30,
+                            color: Colors.white,
+                          )),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                            width: MediaQuery.of(context).size.width * 0.42,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: const Text(
+                              '''Hapus
+data wisata''',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                              overflow: TextOverflow.fade,
+                              softWrap: true,
+                              maxLines: 2,
+                            )),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              indent: 20,
+              endIndent: 20,
+              thickness: 2,
+            ),
+            Flexible(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.amber,
+                    child: const ListTile(
+                      leading: Icon(Icons.add_box),
+                      title: Text(
+                        'Tambah data wisata',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    color: Colors.amber,
+                    child: const ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text('Edit data wisata'),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    color: Colors.amber,
+                    child: const ListTile(
+                      leading: Icon(Icons.delete),
+                      title: Text('Hapus data wisata'),
+                    ),
+                  ),
+                ],
+              ),
+            )),
           ],
         ));
   }
